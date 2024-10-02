f24cd067115185d0af097ba2db54bb26dc396871
dayonad <onadujaao@gmail.com>
2024-10-02 14:49:17 +0100

    update

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,98 +1,106 @@
 export const info = {
-  baseUrl: "https://oladele-john.vercel.app/",
-  name: "Oladele John",
+  baseUrl: "https://onaduja-adedayo.vercel.app/",
+  name: "Onaduja Adedayo",
   jobDescription: "Software Developer",
-  about: `I'm a Software Developer,
-   I love creating things that live on the internet, focused on building dynamic web specific applications. I have successfully completed several web projects, and also gained real world experience while working as software developer as well as collaborating with co-developers. Yet, I learn continuously and never stop exploring new technologies..
-  `,
-/*
+  about: `As a dedicated Software Developer, I specialize in crafting innovative web applications that deliver exceptional user experiences. With a proven track record of successful project completion, I thrive on building dynamic, responsive solutions that meet the evolving demands of the digital landscape. My technical expertise, coupled with a collaborative approach, allows me to excel in fast-paced development environments. I am passionate about staying at the forefront of technological advancements and am continually seeking opportunities to expand my skillset.`,
+  /*
 
-*/ 
-  
+*/
 
   socialMedia: {
-    twitter: "https://twitter.com/coding_wizard",
-    github: "https://github.com/jhon-crypt",
-    email: "johnoladele690@74372gmail.com",
+    twitter: "https://x.com/dayonadd",
+    github: "https://github.com/DAYONAD",
+    email: "onadujaao@gmail.com",
   },
 
   projects: [
     {
-      title: "Devxx",
+      title: "Online Efiko",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1701347546/dev_ztfcxz.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/daiwsn5kz/image/upload/v1727875717/t45obl6tj4po9zkglfrz.png",
       githubUrl: "https://github.com/Jhon-crypt/devx",
       liveUrl: "https://devxx.vercel.app/",
     },
     {
-      title: "EduVerify",
+      title: "LMS",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1694461183/portfolio/eduverify_qzqmjr.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/Certificate-authenticator-",
+      thumbnail:
+        "https://res.cloudinary.com/daiwsn5kz/image/upload/v1727875717/uvnym4uac9tnu1ocxcmb.png",
+      // githubUrl: "https://github.com/Jhon-crypt/Certificate-authenticator-",
       liveUrl: "https://eduverify.brimble.app/",
     },
     {
-      title: "Ludci",
+      title: "DAMKOFT",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1694460976/portfolio/lucid_loqauj.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/ludciX",
+      thumbnail:
+        "https://res.cloudinary.com/daiwsn5kz/image/upload/v1727875717/cx1le3us7fzvj7sb1yon.png",
+      // githubUrl: "https://github.com/Jhon-crypt/ludciX",
       liveUrl: "https://ludcix.brimble.app/",
     },
     {
-      title: "Asistra",
+      title: "E-request",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1694460740/portfolio/asistra_tfgdhu.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/asistra",
+      thumbnail:
+        "https://res.cloudinary.com/daiwsn5kz/image/upload/v1727875715/kqygzzstfj3youk9faz4.png",
+      // githubUrl: "https://github.com/Jhon-crypt/asistra",
       liveUrl: "https://asistra.vercel.app/",
     },
     {
-      title: "Kreate",
+      title: "TAMS V4",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1694460241/portfolio/Kreate_otts4l.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/kreate",
+      thumbnail:
+        "https://res.cloudinary.com/daiwsn5kz/image/upload/v1727875716/jaonw49rxsupt151iwzm.png",
+      // githubUrl: "https://github.com/Jhon-crypt/kreate",
       liveUrl: "https://kreate-ten.vercel.app/",
     },
-    {
-      title: "Oanwtech",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1693040953/oawn_dfdx6u.jpg",
-      githubUrl: "",
-      liveUrl: "https://oanwtech.com/",
-    },
-    {
-      title: "Tasued-Cbt-Practice",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/tasued-cbt-practice",
-      liveUrl: "https://tasued-cbt-practice.vercel.app/",
-    },
-    {
-      title: "To Be Very Honest",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889742/portfolio/tbvh_khfsma.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/tbvh",
-      liveUrl: "https://tbvh.vercel.app/",
-    },
-    {
-      title: "Yofte Ecommerce Landing Page",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889857/portfolio/yofteVue_ntyzih.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/yofteVue",
-      liveUrl: "https://https://yofte-vue.vercel.app.vercel.app/",
-    },
-    {
-      title: "Lifemachines Landing Page",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692891612/portfolio/lifemachines_gkmw6z.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/life-machines",
-      liveUrl: "https://life-machines.vercel.app/",
-    },
-    {
-      title: "Wizard Ai",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692892774/portfolio/wizard_gpfl2j.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/wizardAi",
-      liveUrl: "https://wizardai.vercel.app/",
-    },
+    // {
+    //   title: "Oanwtech",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1693040953/oawn_dfdx6u.jpg",
+    //   githubUrl: "",
+    //   liveUrl: "https://oanwtech.com/",
+    // },
+    // {
+    //   title: "Tasued-Cbt-Practice",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/tasued-cbt-practice",
+    //   liveUrl: "https://tasued-cbt-practice.vercel.app/",
+    // },
+    // {
+    //   title: "To Be Very Honest",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889742/portfolio/tbvh_khfsma.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/tbvh",
+    //   liveUrl: "https://tbvh.vercel.app/",
+    // },
+    // {
+    //   title: "Yofte Ecommerce Landing Page",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889857/portfolio/yofteVue_ntyzih.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/yofteVue",
+    //   liveUrl: "https://https://yofte-vue.vercel.app.vercel.app/",
+    // },
+    // {
+    //   title: "Lifemachines Landing Page",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692891612/portfolio/lifemachines_gkmw6z.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/life-machines",
+    //   liveUrl: "https://life-machines.vercel.app/",
+    // },
+    // {
+    //   title: "Wizard Ai",
+    //   isFeatured: true,
+    //   thumbnail:
+    //     "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692892774/portfolio/wizard_gpfl2j.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/wizardAi",
+    //   liveUrl: "https://wizardai.vercel.app/",
+    // },
   ],
 };
